6a782227a78fcbd14c813a40fee1ec3baaaa8f78
medev21 <benmarteen07@gmail.com>
2018-12-03 14:34:15 -0500

    create main.js file under app/components folder

---
+++ app/components/main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { 
+    StyleSheet, 
+    Text, 
+    View, 
+    TextInput, //to input text
+    ScrollView, //to scroll
+    TouchableOpacity, //touch buttons
+} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+  },
+});
