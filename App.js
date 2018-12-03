316b677f95e4607c526336b86c0cdf15e9f5befb
medev21 <benmarteen07@gmail.com>
2018-12-03 14:21:46 -0500

    remove assets folder and create app folder

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
@@ -13,9 +12,5 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
